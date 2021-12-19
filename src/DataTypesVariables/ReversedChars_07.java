e7bee8eb2a093b29ff0b9f866d5dff505468427d
Teodora Shumanova <t.shumanova@abv.bg>
2021-12-19 14:52:16 +0200

    tasks

---
+++ src/DataTypesVariables/ReversedChars_07.java
@@ -0,0 +1,14 @@
+package DataTypesVariables;
+
+import java.util.Scanner;
+
+public class ReversedChars_07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String symbolOne= scanner.nextLine();
+        String symbolTwo= scanner.nextLine();
+        String symbolThree= scanner.nextLine();
+
+        System.out.printf("%s %s %s",symbolThree,symbolTwo,symbolOne);
+    }
+}
