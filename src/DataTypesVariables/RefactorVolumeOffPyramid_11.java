405125cfad7a07f4b91e780a86ca652ddcd523ea
Teodora Shumanova <t.shumanova@abv.bg>
2021-12-25 15:30:31 +0200

    tasks

---
+++ src/DataTypesVariables/RefactorVolumeOffPyramid_11.java
@@ -0,0 +1,19 @@
+package DataTypesVariables;
+
+import java.util.Scanner;
+
+public class RefactorVolumeOffPyramid_11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Length: ");
+        double lenght = Double.parseDouble(scanner.nextLine());
+        System.out.print("Width: ");
+        double width = Double.parseDouble(scanner.nextLine());
+        System.out.print("Height: ");
+        double height = Double.parseDouble(scanner.nextLine());
+       double volume = (lenght * width * height) / 3;
+        System.out.printf("Pyramid Volume: %.2f", volume);
+
+    }
+}
