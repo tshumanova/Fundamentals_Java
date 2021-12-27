942f81f2d9e60d6c08db00c152747991d60f45fe
Teodora Shumanova <t.shumanova@abv.bg>
2021-12-27 14:28:54 +0200

    tasks

---
+++ src/Arrays/DayOfWeek_01.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class DayOfWeek_01 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String [] daysOfWeek={
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday",
+        };
+        int day=Integer.parseInt(scanner.nextLine());
+        if(day>=1 && day<=7);{
+            System.out.println(daysOfWeek[daysOfWeek.length-1]);
+        }
+
+    }
+}
