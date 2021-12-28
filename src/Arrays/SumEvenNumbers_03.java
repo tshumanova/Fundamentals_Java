af44904d874e06e15e85dd725742d1bd6f9cdf9c
Teodora Shumanova <t.shumanova@abv.bg>
2021-12-28 14:38:16 +0200

    tasks

---
+++ src/Arrays/SumEvenNumbers_03.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class SumEvenNumbers_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
