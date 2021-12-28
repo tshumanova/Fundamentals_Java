81535d3c6848fb94663c84ea29b4eddbc4108648
Teodora Shumanova <t.shumanova@abv.bg>
2021-12-28 14:46:19 +0200

    tasks

--- src/Arrays/SumEvenNumbers_03.java
+++ src/Arrays/SumEvenNumbers_03.java
@@ -1,11 +1,21 @@
 package Arrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SumEvenNumbers_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String inputLine = scanner.nextLine();
+        int[] numArr = Arrays
+                .stream(Arrays.stream(inputLine.split(" "))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+        int sum = 0;
+        for (int i = 0; i < numArr.length ; i++) {
+            
+        }
 
     }
 }
