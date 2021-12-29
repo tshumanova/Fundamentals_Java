5d033c34dfe6af069b4bc984fb9cfa0c6f8ecee4
Teodora Shumanova <t.shumanova@abv.bg>
2021-12-29 14:35:51 +0200

    tasks

--- src/Array_Exercise/Train_01.java
+++ src/Array_Exercise/Train_01.java
@@ -1,7 +1,17 @@
 package Array_Exercise;
 
+import java.util.Scanner;
+
 public class Train_01 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int countWagons = Integer.parseInt(scanner.nextLine());
+        int[] wagons = new int[countWagons];
+        for (int wagon = 0; wagon < countWagons; wagon++) {
+            int countPeople = Integer.parseInt(scanner.nextLine());
+            wagons[wagon] = countPeople;
+
+        }
     }
 }
