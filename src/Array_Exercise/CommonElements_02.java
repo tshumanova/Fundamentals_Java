292205f413f59475e361260226127df7d8e14a64
Teodora Shumanova <t.shumanova@abv.bg>
2021-12-31 13:05:47 +0200

    tasks

---
+++ src/Array_Exercise/CommonElements_02.java
@@ -0,0 +1,14 @@
+package Array_Exercise;
+
+import java.util.Scanner;
+
+public class CommonElements_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String firstLine = scanner.nextLine();
+        String secondLine = scanner.nextLine();
+        String[] array1 = firstLine.split(" ");
+
+    }
+}
