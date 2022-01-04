4ecab36b2dbe20d194e3dfac95e912ce1ce208b0
Teodora Shumanova <t.shumanova@abv.bg>
2022-01-04 14:42:29 +0200

    tasks

--- src/Array_Exercise/ZigZagArrays_03.java
+++ src/Array_Exercise/ZigZagArrays_03.java
@@ -5,12 +5,25 @@ import java.util.Scanner;
 public class ZigZagArrays_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n=Integer.parseInt(scanner.nextLine());
+        int n = Integer.parseInt(scanner.nextLine());
+        String[] firstArray = new String[n];
+        String[] secondArray = new String[n];
         for (int line = 0; line < n; line++) {
-            String input= scanner.nextLine();
-            String firstNumber =input.split(" ")[0];
-            String secondNumber=input.split(" ")[1];
-            System.out.println();
+            String input = scanner.nextLine();
+            String firstNumber = input.split(" ")[0];
+            String secondNumber = input.split(" ")[1];
+            if ((line + 1) % 2 == 1) {
+                firstArray[line] = firstNumber;
+                secondArray[line] = secondNumber;
+            } else {
+                firstArray[line] = secondNumber;
+                secondArray[line] = firstNumber;
+            }
+        }
+        for (String element : firstArray) {
+            System.out.println(element + " ");
+
         }
     }
 }
+
