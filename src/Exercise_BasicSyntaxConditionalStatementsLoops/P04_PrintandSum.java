dab56f0cb085689f33d9024e140841d14e020ae9
Teodora Shumanova <t.shumanova@abv.bg>
2022-01-16 08:14:38 +0200

    tasks

---
+++ src/Exercise_BasicSyntaxConditionalStatementsLoops/P04_PrintandSum.java
@@ -0,0 +1,13 @@
+package Exercise_BasicSyntaxConditionalStatementsLoops;
+
+import java.util.Scanner;
+
+public class P04_PrintandSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int start=Integer.parseInt(scanner.nextLine());
+        int end=Integer.parseInt(scanner.nextLine());
+
+    }
+}
