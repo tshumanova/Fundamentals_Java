6177b16419cc80ce9e2345755f87e758c6f497c7
Teodora Shumanova <t.shumanova@abv.bg>
2022-01-16 08:24:04 +0200

    tasks

--- src/Exercise_BasicSyntaxConditionalStatementsLoops/P04_PrintandSum.java
+++ src/Exercise_BasicSyntaxConditionalStatementsLoops/P04_PrintandSum.java
@@ -6,8 +6,14 @@ public class P04_PrintandSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int start=Integer.parseInt(scanner.nextLine());
-        int end=Integer.parseInt(scanner.nextLine());
-
+        int start = Integer.parseInt(scanner.nextLine());
+        int end = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+            System.out.print(i + " ");
+            sum += i;
+        }
+        System.out.println();
+        System.out.printf("Sum: %d", sum);
     }
 }
---
+++ src/Exercise_BasicSyntaxConditionalStatementsLoops/P05_Login.java
@@ -0,0 +1,11 @@
+package Exercise_BasicSyntaxConditionalStatementsLoops;
+
+import java.util.Scanner;
+
+public class P05_Login {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
