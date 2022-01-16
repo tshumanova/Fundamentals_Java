ca88460e6562dba28f14b633a9a5d8a1de656cc7
Teodora Shumanova <t.shumanova@abv.bg>
2022-01-16 14:41:00 +0200

    tasks

---
+++ src/Exercise_BasicSyntaxConditionalStatementsLoops/P010_PadawanEquipment.java
@@ -0,0 +1,10 @@
+package Exercise_BasicSyntaxConditionalStatementsLoops;
+
+import java.util.Scanner;
+
+public class P010_PadawanEquipment {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
