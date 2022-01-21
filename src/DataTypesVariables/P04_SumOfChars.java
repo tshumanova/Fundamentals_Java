5ea0fd1c2461e5ad5defff52141f4e08d96802ee
Teodora Shumanova <t.shumanova@abv.bg>
2022-01-21 20:39:54 +0200

    tasks

--- src/DataTypesVariables/P04_SumOfChars.java
+++ src/DataTypesVariables/P04_SumOfChars.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 public class P04_SumOfChars {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int n = Integer.parseInt(scanner.nextLine());
-        int sum=0;
-        for (int i = 0; i <n ; i++) {
-           char symbol= scanner.nextLine().charAt(0);
-           sum+=symbol;
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            char symbol = scanner.nextLine().charAt(0);
+            sum += (int)symbol;
         }
+            System.out.println("The sum equals: "+sum);
+
     }
 }
