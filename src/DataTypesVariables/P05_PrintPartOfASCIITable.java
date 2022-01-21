2f8a31ac2ba00897679fc866d74965002c07a6a1
Teodora Shumanova <t.shumanova@abv.bg>
2022-01-21 20:45:09 +0200

    tasks

---
+++ src/DataTypesVariables/P05_PrintPartOfASCIITable.java
@@ -0,0 +1,17 @@
+package DataTypesVariables;
+
+import java.util.Scanner;
+
+public class P05_PrintPartOfASCIITable {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int start = Integer.parseInt(scanner.nextLine());
+        int end = Integer.parseInt(scanner.nextLine());
+        for (int i = start; i <= end; i++) {
+            char symbol =(char)i;
+            System.out.print(symbol +" ");
+        }
+    }
+}
