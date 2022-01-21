e5c6416462873cd85b18ac003a169d48b03dbd07
Teodora Shumanova <t.shumanova@abv.bg>
2022-01-21 20:22:38 +0200

    tasks

---
+++ src/DataTypesVariables/P04_SumOfChars.java
@@ -0,0 +1,15 @@
+package DataTypesVariables;
+
+import java.util.Scanner;
+
+public class P04_SumOfChars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int sum=0;
+        for (int i = 0; i <n ; i++) {
+           char symbol= scanner.nextLine().charAt(0);
+           sum+=symbol;
+        }
+    }
+}
