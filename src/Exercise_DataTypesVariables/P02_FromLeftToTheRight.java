15d3defa27b2a2644f05f131c94e0fbfffded322
Teodora Shumanova <t.shumanova@abv.bg>
2022-01-23 21:14:45 +0200

    tasks

---
+++ src/Exercise_DataTypesVariables/P02_FromLeftToTheRight.java
@@ -0,0 +1,10 @@
+package Exercise_DataTypesVariables;
+
+import java.util.Scanner;
+
+public class P02_FromLeftToTheRight {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
