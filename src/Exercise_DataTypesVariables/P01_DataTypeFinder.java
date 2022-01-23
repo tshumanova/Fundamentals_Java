8a0cb0f11be5466b105dfd57e9129a4aa6d9f1ed
Teodora Shumanova <t.shumanova@abv.bg>
2022-01-23 20:40:21 +0200

    tasks

---
+++ src/Exercise_DataTypesVariables/P01_DataTypeFinder.java
@@ -0,0 +1,12 @@
+package Exercise_DataTypesVariables;
+
+import java.util.Scanner;
+
+public class P01_DataTypeFinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input= scanner.nextLine();
+
+    }
+}
