c1374880b53dfdeb0bacb2ca5d0ea18066c8650a
Teodora Shumanova <t.shumanova@abv.bg>
2022-01-27 17:26:54 +0200

    tasks

--- src/Arrays/P03_SumEvenNumbers.java
+++ src/Arrays/P03_SumEvenNumbers.java
@@ -1,7 +1,23 @@
 package Arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class P03_SumEvenNumbers {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String inputLine = scanner.nextLine();
+        int[] numbers = Arrays
+                .stream(inputLine.split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                sum += numbers[i];
+            }
+        }
+        System.out.println(sum);
     }
 }
