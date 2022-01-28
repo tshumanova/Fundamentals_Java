60e2a828c2fb5234d0c85084949ed90729558ca3
Teodora Shumanova <t.shumanova@abv.bg>
2022-01-28 21:15:05 +0200

    tasks

---
+++ src/Array_Exercise/P08_MagicSum.java
@@ -0,0 +1,9 @@
+package Array_Exercise;
+
+import java.util.Scanner;
+
+public class P08_MagicSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
