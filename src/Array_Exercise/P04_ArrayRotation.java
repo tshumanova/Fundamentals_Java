1cbeb36d502bed82d1a74435a91399672021171d
Teodora Shumanova <t.shumanova@abv.bg>
2022-01-28 21:09:05 +0200

    tasks

--- src/Array_Exercise/P04_ArrayRotation.java
+++ src/Array_Exercise/P04_ArrayRotation.java
@@ -8,5 +8,11 @@ public class P04_ArrayRotation {
 
         String[]array=scanner.nextLine().split(" ");
         int rotations=Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < rotations; i++) {
+            String firstElement=array[0];
+            for (int j = 0; j < array.length-1; j++) {
+                array[j]=array[j+1];
+            }
+        }
     }
 }
