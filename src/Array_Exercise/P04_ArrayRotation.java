36628f4c6c6a74343ecdcc6a0ba1bbe36373e898
Teodora Shumanova <t.shumanova@abv.bg>
2022-01-28 21:12:58 +0200

    tasks

--- src/Array_Exercise/P04_ArrayRotation.java
+++ src/Array_Exercise/P04_ArrayRotation.java
@@ -6,13 +6,15 @@ public class P04_ArrayRotation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[]array=scanner.nextLine().split(" ");
-        int rotations=Integer.parseInt(scanner.nextLine());
+        String[] array = scanner.nextLine().split(" ");
+        int rotations = Integer.parseInt(scanner.nextLine());
         for (int i = 0; i < rotations; i++) {
-            String firstElement=array[0];
-            for (int j = 0; j < array.length-1; j++) {
-                array[j]=array[j+1];
+            String firstElement = array[0];
+            for (int j = 0; j < array.length - 1; j++) {
+                array[j] = array[j + 1];
             }
+            array[array.length - 1] = firstElement;
         }
+        System.out.println(String.join(" ", array));
     }
 }
