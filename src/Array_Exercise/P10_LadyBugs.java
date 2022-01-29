a84823111c853d8b9df7b1800f9777d8a9e27888
Teodora Shumanova <t.shumanova@abv.bg>
2022-01-29 21:59:14 +0200

    tasks

--- src/Array_Exercise/P10_LadyBugs.java
+++ src/Array_Exercise/P10_LadyBugs.java
@@ -1,9 +1,20 @@
 package Array_Exercise;
 
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P10_LadyBugs {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int fielSize=Integer.parseInt(scanner.nextLine());
+        int [] bugPositions= Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(e -> Integer.parseInt(e))
+                .toArray();
+        int[]field=new int[fielSize];
+
+
+
     }
 }
