833e170ce8da90b941c97a6e20daeb6c96e82330
Teodora Shumanova <t.shumanova@abv.bg>
2022-01-29 21:50:29 +0200

    tasks

--- src/Array_Exercise/P10_LadyBugs.java
+++ src/Array_Exercise/P10_LadyBugs.java
@@ -1,4 +1,9 @@
 package Array_Exercise;
 
+import java.util.Scanner;
+
 public class P10_LadyBugs {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
 }
