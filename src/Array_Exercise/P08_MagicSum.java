2fb1a1b32caf2d310497d89ed582107d8acc869e
Teodora Shumanova <t.shumanova@abv.bg>
2022-01-29 17:29:08 +0200

    tasks

--- src/Array_Exercise/P08_MagicSum.java
+++ src/Array_Exercise/P08_MagicSum.java
@@ -6,14 +6,16 @@ import java.util.Scanner;
 public class P08_MagicSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int [] arr = Arrays.stream(scanner.nextLine()
-                .split(" "))
+        int[] arr = Arrays.stream(scanner.nextLine()
+                        .split(" "))
                 .mapToInt(e -> Integer.parseInt(e))
                 .toArray();
-        int number=Integer.parseInt(scanner.nextLine());
-        for (int i = 0; i <arr.length ; i++) {
-            for (int j = i+1; j < ; j++) {
-
+        int number = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == number) {
+                    System.out.println(arr[i] + " " + arr[j]);
+                }
             }
         }
     }
