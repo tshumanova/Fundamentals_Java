0e75ab6fffeae5ae1c77d33972ac9a8ba35ab020
Teodora Shumanova <t.shumanova@abv.bg>
2022-01-29 17:27:05 +0200

    tasks

--- src/Array_Exercise/P08_MagicSum.java
+++ src/Array_Exercise/P08_MagicSum.java
@@ -1,9 +1,20 @@
 package Array_Exercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P08_MagicSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int [] arr = Arrays.stream(scanner.nextLine()
+                .split(" "))
+                .mapToInt(e -> Integer.parseInt(e))
+                .toArray();
+        int number=Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = i+1; j < ; j++) {
+
+            }
+        }
     }
 }
