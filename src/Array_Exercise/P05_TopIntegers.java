3f90470dc275202980060b06cfab78c9ad175740
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-02 18:26:08 +0200

    tasks

---
+++ src/Array_Exercise/P05_TopIntegers.java
@@ -0,0 +1,20 @@
+package Array_Exercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P05_TopIntegers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        int[] numArr = Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt).toArray();
+        int maxNumber = 0;
+        for (int i = 0; i < numArr.length; i++) {
+            boolean isBigger = false;
+            for (int j = i + 1; j < numArr.length; j++) {
+
+            }
+        }
+    }
+}
