36fd2af2b287fb9877a24b343fdd87186e1f341d
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-03 19:09:18 +0200

    tasks

---
+++ src/MoreExercise_DataTypesVariables/P02_PascalTriangle.java
@@ -0,0 +1,15 @@
+package MoreExercise_DataTypesVariables;
+
+import java.util.Scanner;
+
+public class P02_PascalTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n=Integer.parseInt(scanner.nextLine());
+        int [] old_row={1};
+        System.out.println(1);
+
+
+    }
+}
