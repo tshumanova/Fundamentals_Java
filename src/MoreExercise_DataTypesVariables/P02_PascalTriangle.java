5f78ac00ff4ff39cf4ce487b19ac392765b28b75
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-03 19:12:28 +0200

    tasks

--- src/MoreExercise_DataTypesVariables/P02_PascalTriangle.java
+++ src/MoreExercise_DataTypesVariables/P02_PascalTriangle.java
@@ -6,10 +6,19 @@ public class P02_PascalTriangle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n=Integer.parseInt(scanner.nextLine());
-        int [] old_row={1};
+        int n = Integer.parseInt(scanner.nextLine());
+        int[] old_row = {1};
         System.out.println(1);
 
-
+        for (int i = 2; i <= n; i++) {
+            int[] row = new int[i];
+            for (int j = 0; j < i; j++) {
+                if (j == 0 && j == i - 1) row[j] = 1;
+            }
+            for (int nuber:row) {
+                System.out.println();
+            }
+        }
     }
+}
 }
