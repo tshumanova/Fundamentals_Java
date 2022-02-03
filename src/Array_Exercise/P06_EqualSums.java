c41cc96df6e47dd81a264895b1f50b8455a0de3b
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-03 18:57:08 +0200

    tasks

--- src/Array_Exercise/P06_EqualSums.java
+++ src/Array_Exercise/P06_EqualSums.java
@@ -1,9 +1,27 @@
 package Array_Exercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P06_EqualSums {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(e -> Integer.parseInt(e))
+                .toArray();
+
+        boolean  isFound=false;
+        for (int index = 0; index <= numbers.length-1 ; index++) {
+            int sumLeft=0;
+            int sumRight=0;
+            for (int indexLeft = 0; indexLeft <=index-1 ; indexLeft++) {
+                sumLeft+=numbers[indexLeft];
+            }
+            for (int indexRight = index+1; indexRight <= numbers.length-1 ; indexRight++) {
+                sumRight+=numbers[indexRight];
+            }
+
+
+        }
     }
 }
