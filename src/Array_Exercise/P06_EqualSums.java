3916116d6767997a6746726b148ddcd6869b06c4
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-03 18:51:31 +0200

    tasks

---
+++ src/Array_Exercise/P06_EqualSums.java
@@ -0,0 +1,9 @@
+package Array_Exercise;
+
+import java.util.Scanner;
+
+public class P06_EqualSums {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
