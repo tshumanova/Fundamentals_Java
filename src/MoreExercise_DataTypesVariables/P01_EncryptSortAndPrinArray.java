3de4363f2233b559385b8602affdf4cc1555e7bc
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-03 19:38:22 +0200

    tasks

---
+++ src/MoreExercise_DataTypesVariables/P01_EncryptSortAndPrinArray.java
@@ -0,0 +1,12 @@
+package MoreExercise_DataTypesVariables;
+
+import java.util.Scanner;
+
+public class P01_EncryptSortAndPrinArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfStrings=Integer.parseInt(scanner.nextLine());
+        int []numbers=new int[]
+    }
+}
