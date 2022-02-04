0a42258fdb20199b8e87c144772010b54e9a46e0
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-04 21:45:21 +0200

    tasks

---
+++ src/Exercise_Methods/P02_VowelsCount.java
@@ -0,0 +1,9 @@
+package Exercise_Methods;
+
+import java.util.Scanner;
+
+public class P02_VowelsCount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
