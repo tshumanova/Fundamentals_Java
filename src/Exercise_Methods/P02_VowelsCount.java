ed1d1770ca6997a0a1973224f22e2fdd51f23eae
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-04 21:50:14 +0200

    tasks

--- src/Exercise_Methods/P02_VowelsCount.java
+++ src/Exercise_Methods/P02_VowelsCount.java
@@ -5,5 +5,13 @@ import java.util.Scanner;
 public class P02_VowelsCount {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        printVowelsCount(text);
     }
+
+    public static void printVowelsCount(String text){
+        for(int i=0;i<text.length();i++)
+    }
+
 }
