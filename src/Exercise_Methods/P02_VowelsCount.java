d38b3da0cc21e94401080deb48c0d630645578e4
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-04 21:57:43 +0200

    tasks

--- src/Exercise_Methods/P02_VowelsCount.java
+++ src/Exercise_Methods/P02_VowelsCount.java
@@ -10,10 +10,16 @@ public class P02_VowelsCount {
         printVowelsCount(text);
     }
 
-    public static void printVowelsCount(String text){
-        for(int i=0;i<text.length();i++){
-            char symbol=text.toLowerCase().charAt(i);
+    public static void printVowelsCount(String text) {
+        int count = 0;
+        for (int i = 0; i < text.length(); i++) {
+            char symbol = text.toLowerCase().charAt(i);
+            if (symbol == 97 || symbol == 101 || symbol == 105 ||
+                    symbol == 111 || symbol == 117 || symbol == 121) {
+                count++;
+            }
         }
+        System.out.println(count);
     }
 
 }
