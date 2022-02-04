563025fa1cc3fe0b4d0d98476b55df401ece4bfb
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-04 21:23:20 +0200

    tasks

---
+++ src/Exercise_Methods/P01_SmallestOfThreeNumbers.java
@@ -0,0 +1,13 @@
+package Exercise_Methods;
+
+import java.util.Scanner;
+
+public class P01_SmallestOfThreeNumbers {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int num1 = Integer.parseInt(scanner.nextLine());
+        int num2 = Integer.parseInt(scanner.nextLine());
+        int num3 = Integer.parseInt(scanner.nextLine());
+    }
+}
