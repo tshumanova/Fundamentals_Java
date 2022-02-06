686e3ca280b2e8cb6c5dbc4601056b94068d6daf
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-06 18:07:59 +0200

    tasks

--- src/Methods_Lab/P03_PrintingTriangle.java
+++ src/Methods_Lab/P03_PrintingTriangle.java
@@ -5,8 +5,12 @@ import java.util.Scanner;
 public class P03_PrintingTriangle {
     public static void main(String[] args) {
         int number = new Scanner(System.in).nextInt();
-        printIncreasingNumbers(number);
-
+        for (int length = 1; length <= number; length++) {
+            printIncreasingNumbers(length);
+        }
+        for (int length = number - 1; length > 0; length--) {
+            printIncreasingNumbers(length);
+        }
     }
 
     private static void printIncreasingNumbers(int length) {
@@ -16,15 +20,17 @@ public class P03_PrintingTriangle {
                 System.out.print(" ");
             }
         }
+        System.out.println();
+    }
 
-        private static void printArray ( int[] numbers){
-            for (int number : numbers) {
-                System.out.println(number + " ");
-            }
-            System.out.println();
-
-
+    private static void printArray(int[] numbers) {
+        for (int number : numbers) {
+            System.out.println(number + " ");
         }
+        System.out.println();
+
 
     }
+
+}
 
