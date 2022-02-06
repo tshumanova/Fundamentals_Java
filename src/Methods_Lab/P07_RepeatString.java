5cbe5ea9508489b5ac93845a2548891a75cdf306
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-06 18:40:02 +0200

    tasks

--- src/Methods_Lab/P07_RepeatString.java
+++ src/Methods_Lab/P07_RepeatString.java
@@ -2,6 +2,15 @@ package Methods_Lab;
 
 public class P07_RepeatString {
     public static void main(String[] args) {
+        String s = "hello";
 
+
+        int repetitions = 3;
+        String repeated = repeatString(s, repetitions);
+        System.out.println(repeated);
+    }
+
+    private static String repeatString(String s, int repetitions) {
+        return null;
     }
 }
