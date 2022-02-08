74ca5cfd002301ed00aa7a41658105ed39757798
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-08 23:42:05 +0200

    tasks

--- src/Methods_Lab/P02_Grades.java
+++ src/Methods_Lab/P02_Grades.java
@@ -1,7 +1,19 @@
 package Methods_Lab;
 
+import java.util.Scanner;
+
 public class P02_Grades {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        printInWords(Double.parseDouble(scanner.nextLine()));
     }
+
+    private static void printInWords(double grade) {
+        String gradeImWords = "";
+        if(grade>=2 && grade<=2.99){
+            gradeImWords="Fail";
+        }
+        System.out.println(gradeImWords);
+    }
+
 }
