dcfa91886b142ddd626792f6f17db0b881c2cfe9
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-09 19:43:28 +0200

    tasks

--- src/Methods_Lab/P06_CalculateRectangleArea.java
+++ src/Methods_Lab/P06_CalculateRectangleArea.java
@@ -6,5 +6,13 @@ public class P06_CalculateRectangleArea {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double width = Double.parseDouble(scanner.nextLine());
+        double height = Double.parseDouble(scanner.nextLine());
+        double area = calcRectangleArea(width, height);
+        System.out.printf("%.0f%n", area);
+    }
+
+    private static double calcRectangleArea(double width, double height) {
+        return width * height;
     }
 }
