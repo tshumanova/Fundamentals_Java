5272de2fafd7827d041beda6d85c29cb1db93fe4
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-09 19:38:20 +0200

    tasks

---
+++ src/Methods_Lab/P06_CalculateRectangleArea.java
@@ -0,0 +1,10 @@
+package Methods_Lab;
+
+import java.util.Scanner;
+
+public class P06_CalculateRectangleArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
