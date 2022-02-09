27a0a22a65ddf5ea6ff2b2c4c4cef2be87c612ab
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-09 19:34:06 +0200

    tasks

--- src/Methods_Lab/P02_Grades.java
+++ src/Methods_Lab/P02_Grades.java
@@ -10,10 +10,22 @@ public class P02_Grades {
 
     private static void printInWords(double grade) {
         String gradeImWords = "";
-        if(grade>=2 && grade<=2.99){
-            gradeImWords="Fail";
+        if (grade >= 2 && grade <= 2.99) {
+            gradeImWords = "Fail";
+            System.out.println(gradeImWords);
+        } else if (grade >= 3.00 && grade <= 3.49) {
+            gradeImWords = "Poor";
+            System.out.println(gradeImWords);
+        } else if (grade >= 3.50 && grade <= 4.49) {
+            gradeImWords = "Good";
+            System.out.println(gradeImWords);
+        } else if (grade >= 4.50 && grade <= 5.49) {
+            gradeImWords = "Very good";
+            System.out.println(gradeImWords);
+        } else if (grade >= 5.50 && grade <= 6.00) {
+            gradeImWords = "Excellent";
+            System.out.println(gradeImWords);
         }
-        System.out.println(gradeImWords);
-    }
 
+    }
 }
