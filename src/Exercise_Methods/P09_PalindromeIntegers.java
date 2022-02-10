6b8d3023f3ff03c60127e6b5981397d82a71e833
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-10 20:49:17 +0200

    tasks

--- src/Exercise_Methods/P09_PalindromeIntegers.java
+++ src/Exercise_Methods/P09_PalindromeIntegers.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 public class P09_PalindromeIntegers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String command= scanner.nextLine();
+        String command = scanner.nextLine();
 
-        while(!command.equals("END")){
+        while (!command.equals("END")) {
             System.out.println(isPalindrome(command));
-            command= scanner.nextLine();
+            command = scanner.nextLine();
         }
     }
 
     private static boolean isPalindrome(String num) {
-        boolean isPalindrome=true;
+        boolean isPalindrome = true;
+        for (int i = 0; i < num.length() / 2; i++) {
+            if (num.charAt(i) != num.charAt(num.length() - 1 - i)) {
+            }
+        }
     }
-}
