d11765dc78a84647909f81da78d38d8e00ba1524
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-10 20:51:04 +0200

    tasks

--- src/Exercise_Methods/P09_PalindromeIntegers.java
+++ src/Exercise_Methods/P09_PalindromeIntegers.java
@@ -17,6 +17,9 @@ public class P09_PalindromeIntegers {
         boolean isPalindrome = true;
         for (int i = 0; i < num.length() / 2; i++) {
             if (num.charAt(i) != num.charAt(num.length() - 1 - i)) {
+                isPalindrome = false;
             }
         }
+        return isPalindrome;
     }
+}
