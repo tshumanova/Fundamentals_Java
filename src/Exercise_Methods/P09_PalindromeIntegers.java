80381838ae5239a526a7391886c87130bd1ea6ea
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-10 20:44:54 +0200

    tasks

--- src/Exercise_Methods/P09_PalindromeIntegers.java
+++ src/Exercise_Methods/P09_PalindromeIntegers.java
@@ -1,7 +1,15 @@
 package Exercise_Methods;
 
+import java.util.Scanner;
+
 public class P09_PalindromeIntegers {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String command= scanner.nextLine();
 
+        while(!command.equals("END")){
+            System.out.println(isPalindrome(command));
+            command= scanner.nextLine();
+        }
     }
 }
