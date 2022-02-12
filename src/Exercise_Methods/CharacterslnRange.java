d09132775b2e607f938d62070235c7af2208669c
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-12 13:33:34 +0200

    tasks

---
+++ src/Exercise_Methods/CharacterslnRange.java
@@ -0,0 +1,19 @@
+package Exercise_Methods;
+
+import java.util.Scanner;
+
+public class CharacterslnRange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char start = scanner.nextLine().charAt(0);
+        char end = scanner.nextLine().charAt(0);
+
+        printCharacter(start, end);
+
+    }
+
+    public static void printCharacter(char start, char end) {
+
+    }
+}
