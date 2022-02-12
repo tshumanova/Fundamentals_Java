64ca4511599f8ed4c8c21ab7415180907677ad88
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-12 13:27:50 +0200

    tasks

--- src/Exercise_Methods/VowelsCount.java
+++ src/Exercise_Methods/VowelsCount.java
@@ -10,9 +10,15 @@ public class VowelsCount {
     }
 
     public static void printVowelsCount(String text) {
+        int count = 0;
         for (int i = 0; i < text.length(); i++) {
             char symbol = text.toLowerCase().charAt(i);
+            if (symbol == 97 || symbol == 101
+                    || symbol == 105 || symbol == 111
+                    || symbol == 117 || symbol == 121) {
+                count++;
+            }
         }
-
+        System.out.println(count);
     }
 }
