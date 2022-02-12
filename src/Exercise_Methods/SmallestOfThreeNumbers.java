a6c8bd4caafafe883d9b87b6c49c18291c582c41
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-12 12:32:36 +0200

    tasks

---
+++ src/Exercise_Methods/SmallestOfThreeNumbers.java
@@ -0,0 +1,21 @@
+package Exercise_Methods;
+
+import java.util.Scanner;
+
+public class SmallestOfThreeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = Integer.parseInt(scanner.nextLine());
+        int num2 = Integer.parseInt(scanner.nextLine());
+        int num3 = Integer.parseInt(scanner.nextLine());
+
+        int smallestNum = getSmallestNumber(num1, num2, num3);
+        System.out.println(smallestNum);
+    }
+
+    private static int getSmallestNumber(int num1, int num2, int num3) {
+        int numbers[]={num1,num2,num3};
+
+    }
+}
