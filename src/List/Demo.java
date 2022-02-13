3e2ae31bda62ef51e899c3fa197e565cdc22ee6d
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-13 21:06:51 +0200

    tasks

--- src/List/Demo.java
+++ src/List/Demo.java
@@ -8,9 +8,13 @@ public class Demo {
     public static void main(String[] args) {
         List<String> words = new ArrayList<String>();
         Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        while (!word.equals(".")) {
+            words.add(word);
+            word = scanner.nextLine();
+        }
 
-
-        for (int i = words.size()-1; i>=0 ; i++) {
+        for (int i = words.size() - 1; i >= 0; i++) {
             System.out.println(words.get(i));
         }
         /*some
@@ -20,6 +24,10 @@ public class Demo {
         one
         brother
         .
+        tghjkl
+        fjdksd
+        fdfss
+
          */
 
     }
