3d7ef57f50279e33ad5509912d376c0991af06ef
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-14 15:20:38 +0200

    tasks

--- src/List/P01_SumAdjacentEqualNumbers.java
+++ src/List/P01_SumAdjacentEqualNumbers.java
@@ -1,9 +1,15 @@
 package List;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class P01_SumAdjacentEqualNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        List<Double>numbers= Arrays.stream(scanner.nextLine().split(" "))
+                .map(Double::parseDouble).collect(Collectors.toList());
     }
 }
