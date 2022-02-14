6402a44d43dbb5f34db746e67a790ef8a84c2a2a
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-14 15:10:23 +0200

    tasks

---
+++ src/List/P01_SumAdjacentEqualNumbers.java
@@ -0,0 +1,9 @@
+package List;
+
+import java.util.Scanner;
+
+public class P01_SumAdjacentEqualNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
