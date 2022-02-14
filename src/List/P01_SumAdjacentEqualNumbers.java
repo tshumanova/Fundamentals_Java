c01197e1a43047d705959f58735778e66895233c
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-14 15:24:50 +0200

    tasks

--- src/List/P01_SumAdjacentEqualNumbers.java
+++ src/List/P01_SumAdjacentEqualNumbers.java
@@ -9,7 +9,14 @@ public class P01_SumAdjacentEqualNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<Double>numbers= Arrays.stream(scanner.nextLine().split(" "))
+        List<Double> numbers = Arrays.stream(scanner.nextLine().split(" "))
                 .map(Double::parseDouble).collect(Collectors.toList());
+        for (int i = 0; i < numbers.size() - 1; i++)
+            if (numbers.get(i).equals(numbers.get(i + 1))) {
+                numbers.set(i, numbers.get(i) + numbers.get(i + 1));
+                numbers.remove(i + 1);
+                i = -1;
+
+            }
     }
 }
