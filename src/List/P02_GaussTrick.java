000895fbdfab434cd666cdac3e3fafd8982a42ea
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-15 17:57:44 +0200

    tasks

---
+++ src/List/P02_GaussTrick.java
@@ -0,0 +1,11 @@
+package List;
+
+import java.util.Scanner;
+
+public class P02_GaussTrick {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
