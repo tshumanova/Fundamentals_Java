e540bbe8c8edaf61dce7dc8c1328261bb72947a5
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-16 14:00:16 +0200

    tasks

---
+++ src/List/P03_MergingLists.java
@@ -0,0 +1,18 @@
+package List;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class P03_MergingLists {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        List<Integer> numbersA = new ArrayList<>(Arrays.asList(10, 6 , 3, 2, 1));
+        List<Integer> numbersB = new ArrayList<>(Arrays.asList(9, 8, 4, 0));
+
+        List<Integer> merged = new ArrayList<>();
+    }
+}
