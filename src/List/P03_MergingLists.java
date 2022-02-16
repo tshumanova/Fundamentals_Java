6af40d471779c8158ac7936462fcc4016cec5f9b
Teodora Shumanova <t.shumanova@abv.bg>
2022-02-16 14:43:15 +0200

    tasks

--- src/List/P03_MergingLists.java
+++ src/List/P03_MergingLists.java
@@ -10,9 +10,24 @@ public class P03_MergingLists {
         Scanner scanner = new Scanner(System.in);
 
 
-        List<Integer> numbersA = new ArrayList<>(Arrays.asList(10, 6 , 3, 2, 1));
-        List<Integer> numbersB = new ArrayList<>(Arrays.asList(9, 8, 4, 0));
+        List<Integer> sortedA = new ArrayList<>(Arrays.asList(10, 6, 3, 2, 1));
+        List<Integer> sortedB = new ArrayList<>(Arrays.asList(9, 8, 4, 0));
 
+        int indexA = 0;
+        int indexB = 0;
         List<Integer> merged = new ArrayList<>();
+        while (indexA < sortedA.size() || indexB < sortedB.size()) {
+            if (indexA < sortedA.size() && sortedA.get(indexA) > sortedB.get(indexB)) {
+                merged.add(sortedA.get(indexA));
+                indexA++;
+            } else {
+                merged.add(sortedB.get(indexB));
+                indexB++;
+            }
+        }
+        for (int number : merged) {
+            System.out.print(number + " ");
+        }
+
     }
 }
