d71b0804403fc7ed8196b568251dc07f2b574285
Teodora Shumanova <t.shumanova@abv.bg>
2022-03-02 18:06:46 +0200

    tasks

---
+++ src/List/P02_Gauss.java
@@ -0,0 +1,25 @@
+package List;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class P02_Gauss {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numList = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < numList.size() ; i++) {
+            int firstNum= numList.get(i);
+            int secondNum=numList.get(numList.size()-1-i);
+
+            numList.set(i,firstNum+secondNum);
+            numList.remove(numList.size()-1);
+        }
+    }
+}
