a6724508ef62b91068d65e39cc191d6bba6408a7
Teodora Shumanova <t.shumanova@abv.bg>
2022-03-03 11:28:17 +0200

    tasks

--- src/Exercise_Methods/P03_CharactersInRange.java
+++ src/Exercise_Methods/P03_CharactersInRange.java
@@ -1,7 +1,25 @@
 package Exercise_Methods;
 
+import java.util.Scanner;
+
 public class P03_CharactersInRange {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        char symbolOne = scanner.nextLine().charAt(0);
+        char symbolTwo = scanner.nextLine().charAt(0);
+        printSequenceOfChars(symbolOne, symbolTwo);
+    }
+
+    private static void printSequenceOfChars(char symbolOne, char symbolTwo) {
+        if (symbolOne < symbolTwo) {
+            for (char symbol = (char) (symbolOne + 1); symbol < symbolTwo; symbol++) {
+                System.out.print(symbol + " ");
+            }
+        } else {
+            for (char symbol = (char) (symbolTwo + 1); symbol < symbolOne; symbol++) {
+                System.out.print(symbol + " ");
+            }
+        }
     }
 }
