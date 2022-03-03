ec3e09a9bd02b6570046e01d64c9cd1719e12a50
Teodora Shumanova <t.shumanova@abv.bg>
2022-03-03 11:46:17 +0200

    tasks

--- src/Exercise_Methods/P04_PasswordValidator.java
+++ src/Exercise_Methods/P04_PasswordValidator.java
@@ -5,7 +5,25 @@ import java.util.Scanner;
 public class P04_PasswordValidator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String password = scanner.nextLine();
 
+        boolean isPasswordValidLength = isValidLength(password);
+        if (!isPasswordValidLength) {
+            System.out.println("Password must be between 6 and 10 characters");
+        }
+        boolean isValidPasswordContent = isValidContent(password);
+        if (!isValidPasswordContent) {
+            System.out.println("Password must consist only of letters and digits");
+        }
+
+        if (isPasswordValidLength && isValidPasswordContent && isValidPasswordCountDigits) {
+            System.out.println("Password is vadlid");
+        }
+    }
+
+    private static boolean isValidLength(String password);
+
+    {
 
     }
 }
