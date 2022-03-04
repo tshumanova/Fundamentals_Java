f4114b9a4b2fae985275b0465ec7253b9ea27969
Teodora Shumanova <t.shumanova@abv.bg>
2022-03-04 14:19:26 +0200

    tasks

---
+++ src/Exercise_Lists/P07_AppendArrays.java
@@ -0,0 +1,26 @@
+package Exercise_Lists;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class P07_AppendArrays {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        List<String> listSeparatedByPipe = Arrays
+                .stream(input.split("\\|"))
+                .collect(Collectors.toList());
+
+        Collections.reverse(listSeparatedByPipe);
+        System.out.println(listSeparatedByPipe.toString()
+                .replace("[", "")
+                .replace("]", "")
+                .trim()
+                .replaceAll(",", "")
+                .replaceAll("\\s+", " "));
+    }
+}
