340e71c183fed0b291d4188dafd1fba67ec2eec1
Teodora Shumanova <t.shumanova@abv.bg>
2022-03-29 14:32:31 +0300

    tasks

--- src/Exercise_AssociativeArrays/P03_LegendaryFarming.java
+++ src/Exercise_AssociativeArrays/P03_LegendaryFarming.java
@@ -1,9 +1,54 @@
 package Exercise_AssociativeArrays;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class P03_LegendaryFarming {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        Map<String, Integer> materials = new HashMap<>();
+        materials.put("shards", 0);
+        materials.put("fragments", 0);
+        materials.put("motes", 0);
+
+        Map<String, Integer> junks = new TreeMap<>();
+        boolean isOver = false;
+        while (!isOver) {
+            String input = scanner.nextLine();
+            String[] inputData = input.split(" ");
+            for (int index = 0; index < inputData.length; index += 2) {
+                int quantity = Integer.parseInt(inputData[index]);
+                String material = inputData[index + 1].toLowerCase();
+
+                if (material.equals("shards") || material.equals("fragments") || material.equals("motes")) {
+                    int currentQuantity = materials.get(material);
+                    materials.put(material, currentQuantity + quantity);
+                    if (materials.get(material) >= 250) {
+                        if (material.equals("shards")) {
+                            System.out.println("Shadowmourne obtained!");
+                        } else if (material.equals("fragments")) {
+                            System.out.println("Valanyr obtained!");
+                        } else {
+                            System.out.println("Dragonwrath obtained!");
+                        }
+                        materials.put(material, materials.get(material) - 250);
+                        isOver = true;
+                        break;
+                    }
+                } else {
+                    if (!junks.containsKey(material)) {
+                        junks.put(material, quantity);
+                    } else {
+                        int currentQuantity = junks.get(material);
+                        junks.put(material, currentQuantity + quantity);
+                    }
+                }
+            }
+        }
+        materials.entrySet().stream().sorted((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()))
+                .forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
+
+        junks.entrySet().forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
     }
 }
+
