b9420786805b151a55dd322501db00342c912eb7
Teodora Shumanova <t.shumanova@abv.bg>
2022-03-30 10:15:21 +0300

    tasks

---
+++ src/Text_Processing_Lab/P02_RepeatStrings.java
@@ -0,0 +1,32 @@
+package Text_Processing_Lab;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class P02_RepeatStrings {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] wordsArr = scanner.nextLine().split(" ");
+
+        List<String> resultList = new ArrayList<>();
+
+        for (String s : wordsArr) {
+            resultList.add(repeatString(s, s.length()));
+        }
+        System.out.println(String.join("", resultList));
+    }
+
+    public static String repeatString(String word, int count) {
+        String[] repeatArr = new String[count];
+        for (int i = 0; i < count; i++) {
+            repeatArr[i] = word;
+        }
+        return String.join("", repeatArr);
+    }
+
+}
