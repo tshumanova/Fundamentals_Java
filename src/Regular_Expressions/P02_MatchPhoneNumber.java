1c68d8388c5445a641e7013adda2577f436b1368
Teodora Shumanova <t.shumanova@abv.bg>
2022-03-31 11:05:49 +0300

    tasks

---
+++ src/Regular_Expressions/P02_MatchPhoneNumber.java
@@ -0,0 +1,25 @@
+package Regular_Expressions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class P02_MatchPhoneNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String regex = "\\+359([ -])2\\1\\d{3}\\1\\d{4}\\b";
+        String text = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        List<String> phoneNumbersList = new ArrayList<>();
+        while (matcher.find()) {
+            phoneNumbersList.add(matcher.group());
+        }
+        System.out.println(String.join(", ", phoneNumbersList));
+    }
+}
