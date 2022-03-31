f7a53d5eabe7fca8523d11af5f8519847abedf70
Teodora Shumanova <t.shumanova@abv.bg>
2022-03-31 11:22:44 +0300

    tasks

---
+++ src/Regular_Expressions/P03_MatchDates.java
@@ -0,0 +1,25 @@
+package Regular_Expressions;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class P03_MatchDates {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String regex = "(?<day>\\d{2})([.\\/-])(?<month>[A-Z][a-z]{2})\\2(?<years>\\d{4})";
+        String text = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            //System.out.println(matcher.group());
+            System.out.printf("Day: %s, Month: %s, Year: %s%n",
+                    matcher.group("day"),
+                    matcher.group("month"),
+                    matcher.group("years"));
+        }
+    }
+}
