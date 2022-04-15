b1a97436e859a4655582a5701836159c5b8a4286
Teodora Shumanova <t.shumanova@abv.bg>
2022-04-15 13:47:39 +0300

    tasks

--- src/Exercise_Methods/PasswordValidator_04.java
+++ src/Exercise_Methods/PasswordValidator_04.java
@@ -5,7 +5,14 @@ import java.util.Scanner;
 public class PasswordValidator_04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        String password = scanner.nextLine();
+        //проверка за дължина -> [6;10]
+        //проверка за съдържанието -. само букви и цифри
+        //проверка за брой на цифрите -> минимум 2
     }
+        //метод за валидиране на дължината
+        //метод за валидиране а съдържанието
+        //метод за валидиране на брой на цифрите
+
+
 }
