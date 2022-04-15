d6b5ff5d6da63725420544083212895300116bb5
Teodora Shumanova <t.shumanova@abv.bg>
2022-04-15 13:40:10 +0300

    tasks

---
+++ src/Exercise_Methods/PasswordValidator_04.java
@@ -0,0 +1,11 @@
+package Exercise_Methods;
+
+import java.util.Scanner;
+
+public class PasswordValidator_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
