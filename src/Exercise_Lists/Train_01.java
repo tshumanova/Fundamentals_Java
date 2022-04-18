10862db8ece9bce0140511eb845fb5b9952733b0
Teodora Shumanova <t.shumanova@abv.bg>
2022-04-18 16:35:02 +0300

    tasks

---
+++ src/Exercise_Lists/Train_01.java
@@ -0,0 +1,11 @@
+package Exercise_Lists;
+
+import java.util.Scanner;
+
+public class Train_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
