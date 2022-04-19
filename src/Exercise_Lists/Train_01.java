ca79b83e70386928d28d6a2ed20554bc2d791726
Teodora Shumanova <t.shumanova@abv.bg>
2022-04-19 16:04:30 +0300

    tasks

--- src/Exercise_Lists/Train_01.java
+++ src/Exercise_Lists/Train_01.java
@@ -16,9 +16,24 @@ public class Train_01 {
         for (int i = 0; i <= input.length; i++) {
             train.add(Integer.parseInt(input[i]));
         }
-        while (!input[0].equals("end")){
-            if(!input[0].equals("end")){
-                input=scanner.nextLine().split(" ");
+        while (!input[0].equals("end")) {
+            if (!input[0].equals("end")) {
+                input = scanner.nextLine().split(" ");
+            } else {
+                break;
+            }
+            if (input[0].equals("Add")) {
+                train.add(Integer.parseInt(input[1]));
+
+            } else if (!input[0].equals("end")) {
+                for (int i = 0; i < train.size(); i++) {
+                    int sum = train.get(i) + Integer.parseInt(input[0]);
+                    if (capacity >= sum) {
+
+                        train.set(i, sum);
+                    }
+
+                }
             }
         }
 
