5a80691a1db770ac87743d829ed02fdbe8df99d9
Teodora Shumanova <t.shumanova@abv.bg>
2022-04-20 14:18:31 +0300

    tasks

--- src/Exercise_ObjectsAndClasses/Article_02.java
+++ src/Exercise_ObjectsAndClasses/Article_02.java
@@ -1,5 +1,7 @@
 package Exercise_ObjectsAndClasses;
 
+import java.util.Scanner;
+
 public class Article_02 {
 
     //полета
@@ -23,5 +25,11 @@ public class Article_02 {
 
     public void setAuthor(String newAuthor) {
         this.author = newAuthor;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
     }
 }
