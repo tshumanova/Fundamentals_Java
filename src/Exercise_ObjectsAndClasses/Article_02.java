70a5a4d30f3c874aee9249c267884eef2d3edbc2
Teodora Shumanova <t.shumanova@abv.bg>
2022-04-20 14:12:27 +0300

    tasks

--- src/Exercise_ObjectsAndClasses/Article_02.java
+++ src/Exercise_ObjectsAndClasses/Article_02.java
@@ -4,11 +4,12 @@ public class Article_02 {
 
     //полета
     private String title;
-
     private String content;
     private String author;
 
-
+    public Article_02(String title, String content, String author) {
+    this.title=title;
+    }
     //методи
 //конструктор
 }
