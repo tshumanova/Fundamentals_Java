e21e9687103598f4d11ebdee32b57eaf8e3e7898
Teodora Shumanova <t.shumanova@abv.bg>
2022-04-20 13:30:58 +0300

    tasks

---
+++ src/Exercise_ObjectsAndClasses/Article_02.java
@@ -0,0 +1,14 @@
+package Exercise_ObjectsAndClasses;
+
+public class Article_02 {
+
+    //полета
+    private String title;
+
+    private String content;
+    private String author;
+
+
+    //методи
+//конструктор
+}
