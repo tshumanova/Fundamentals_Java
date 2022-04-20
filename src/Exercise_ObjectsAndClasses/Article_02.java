a4862de1fe8101083a6b1f0fe8445ea3f8ba7f48
Teodora Shumanova <t.shumanova@abv.bg>
2022-04-20 14:21:35 +0300

    tasks

--- src/Exercise_ObjectsAndClasses/Article_02.java
+++ src/Exercise_ObjectsAndClasses/Article_02.java
@@ -3,33 +3,34 @@ package Exercise_ObjectsAndClasses;
 import java.util.Scanner;
 
 public class Article_02 {
+    public static class Article {
+        //полета
+        private String title;
+        private String content;
+        private String author;
 
-    //полета
-    private String title;
-    private String content;
-    private String author;
+        public Article(String title, String content, String author) {
+            this.title = title;
+            this.content = content;
+            this.author = author;
+        }
 
-    public Article_02(String title, String content, String author) {
-        this.title = title;
-        this.content = content;
-        this.author = author;
-    }
+        public void setTitle(String newTitle) {
+            this.title = newTitle;
+        }
 
-    public void setTitle(String newTitle) {
-        this.title = newTitle;
-    }
+        public void setContent(String newContent) {
+            this.content = newContent;
+        }
 
-    public void setContent(String newContent) {
-        this.content = newContent;
-    }
-
-    public void setAuthor(String newAuthor) {
-        this.author = newAuthor;
+        public void setAuthor(String newAuthor) {
+            this.author = newAuthor;
+        }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String[] articlesParts = scanner.nextLine().split(" ");
 
     }
 }
