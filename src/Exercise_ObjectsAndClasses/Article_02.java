7fb99edbb6f18609535eead01ee2d64b00592ff8
Teodora Shumanova <t.shumanova@abv.bg>
2022-04-20 14:25:43 +0300

    tasks

--- src/Exercise_ObjectsAndClasses/Article_02.java
+++ src/Exercise_ObjectsAndClasses/Article_02.java
@@ -32,5 +32,9 @@ public class Article_02 {
         Scanner scanner = new Scanner(System.in);
         String[] articlesParts = scanner.nextLine().split(" ");
 
+
+        String title = articlesParts[0];
+        String content = articlesParts[1];
+        String author = articlesParts[2];
     }
 }
