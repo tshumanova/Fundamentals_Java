88513ccddcd8d1a47f2911a27cbf2c7c20c431a9
Teodora Shumanova <t.shumanova@abv.bg>
2022-04-21 15:13:00 +0300

    tasks

--- src/Exercise_Regular/Funiture.java
+++ src/Exercise_Regular/Funiture.java
@@ -1,7 +1,13 @@
 package Exercise_Regular;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Funiture {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        Pattern pattern = Pattern.compile("[>]{2}(?<name>[\\w]+)[<]{2}(?<price>[\\d.]+)!(?<quantity>[\\d]+)");
 
 
     }
