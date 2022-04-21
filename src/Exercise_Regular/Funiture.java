7b3c3acaa9861aa46a22418117ef845f02ea50a3
Teodora Shumanova <t.shumanova@abv.bg>
2022-04-21 15:14:16 +0300

    tasks

--- src/Exercise_Regular/Funiture.java
+++ src/Exercise_Regular/Funiture.java
@@ -1,5 +1,7 @@
 package Exercise_Regular;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -7,7 +9,10 @@ public class Funiture {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String line = scanner.nextLine();
+
         Pattern pattern = Pattern.compile("[>]{2}(?<name>[\\w]+)[<]{2}(?<price>[\\d.]+)!(?<quantity>[\\d]+)");
+        List<String> purchases = new LinkedList<>();
+        double totalMoney = 0;
 
 
     }
