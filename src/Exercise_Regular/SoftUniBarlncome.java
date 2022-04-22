5b9e498638bc8e675a05106501c81319d357af2c
Teodora Shumanova <t.shumanova@abv.bg>
2022-04-22 21:56:42 +0300

    tasks

---
+++ src/Exercise_Regular/SoftUniBarlncome.java
@@ -0,0 +1,9 @@
+package Exercise_Regular;
+
+import java.util.Scanner;
+
+public class SoftUniBarlncome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
