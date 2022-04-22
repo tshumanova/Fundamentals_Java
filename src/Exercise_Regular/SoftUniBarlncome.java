dfc9f25c47512e667418319962db20edfa997e56
Teodora Shumanova <t.shumanova@abv.bg>
2022-04-22 21:58:50 +0300

    tasks

--- src/Exercise_Regular/SoftUniBarlncome.java
+++ src/Exercise_Regular/SoftUniBarlncome.java
@@ -1,9 +1,14 @@
 package Exercise_Regular;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class SoftUniBarlncome {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        Pattern pattern = Pattern.compile("%(?<name>[A-Z][a-z]+)%[^|$.]*?<(?<product>\\w+)>[^|$.]*?\\|(?<count>\\d+)\\|[^|$.]*?(?<price>\\d+\\.?\\d+)\\$");
+        double income = 0;
+
     }
 }
