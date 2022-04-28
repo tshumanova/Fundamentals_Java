f160f779e49722d95baf33d21714171916669d3d
Teodora Shumanova <t.shumanova@abv.bg>
2022-04-28 16:24:15 +0300

    tasks

---
+++ src/Exercise_AssociativeArrays/LegendaryFarming.java
@@ -0,0 +1,11 @@
+package Exercise_AssociativeArrays;
+
+import java.util.Scanner;
+
+public class LegendaryFarming {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
