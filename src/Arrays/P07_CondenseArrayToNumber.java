3b8b9e0a709359e0e99cac223a2b494397f4fae2
Teodora Shumanova <t.shumanova@abv.bg>
2022-04-29 15:13:25 +0300

    tasks

--- src/Arrays/P07_CondenseArrayToNumber.java
+++ src/Arrays/P07_CondenseArrayToNumber.java
@@ -4,6 +4,10 @@ public class P07_CondenseArrayToNumber {
     public static void main(String[] args) {
 
         int[] numbers = new int[]{5, 0, 4, 1, 2};
-
+        for (int repetition = 0; repetition < numbers.length - 1; repetition++) {
+            for (int i = 0; i < numbers.length-1; i++) {
+                
+            }
+        }
     }
 }
