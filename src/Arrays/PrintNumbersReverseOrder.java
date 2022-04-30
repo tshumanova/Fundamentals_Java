6b206aabe9ee84fc9db8f7573a363b38b343ecd9
Teodora Shumanova <t.shumanova@abv.bg>
2022-04-30 22:58:34 +0300

    tasks

--- src/Arrays/PrintNumbersReverseOrder.java
+++ src/Arrays/PrintNumbersReverseOrder.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class PrintNumbersReverseOrder {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int number = Integer.parseInt(scanner.nextLine());
     }
 }
