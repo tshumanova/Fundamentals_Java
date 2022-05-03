0975907c9442c523a55fb4ef4e879b91e1d9b730
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-03 10:53:11 +0300

    tasks

---
+++ src/Arrays/T04_ReverseArrayOfString.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class T04_ReverseArrayOfString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String elements = scanner.nextLine();
+        String[] arrString = elements.split(" ");
+        for (int i = 0; i < arrString.length / 2; i++) {
+            String oldElement=arrString[i];
+
+        }
+    }
+}
