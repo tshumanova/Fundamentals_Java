991a2be162337a6c3f76bbeffe1f7ae94eac2b4c
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-03 11:54:36 +0300

    tasks

--- src/Arrays/T06_EqualArrays.java
+++ src/Arrays/T06_EqualArrays.java
@@ -6,14 +6,24 @@ import java.util.Scanner;
 public class T06_EqualArrays {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int[] firstArr = Arrays
                 .stream(scanner.nextLine().split(" "))
                 .mapToInt(e -> Integer.parseInt(e))
                 .toArray();
+
         int[] secondArr = Arrays
                 .stream(scanner.nextLine().split(" "))
                 .mapToInt(e -> Integer.parseInt(e))
                 .toArray();
 
+        boolean arrayAreEquals;
+        if (firstArr.length != secondArr.length) {
+            arrayAreEquals = false;
+        } else {
+            arrayAreEquals = true;
+            for (int i = 0; i < firstArr.length; i++) {
+
+            }
+        }
     }
-}
