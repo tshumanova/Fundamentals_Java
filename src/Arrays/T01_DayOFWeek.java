71ec91ef6ec5f3711243e4856526a0feb13fdcfc
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-03 09:39:37 +0300

    tasks

---
+++ src/Arrays/T01_DayOFWeek.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class T01_DayOFWeek {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int day = Integer.parseInt(scanner.nextLine());
+        String[] days = new String[]{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+    }
+}
