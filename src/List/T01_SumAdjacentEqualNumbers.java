f0112273dc04761f021de056667cac5ea663be3f
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-04 10:20:25 +0300

    tasks

--- src/List/T01_SumAdjacentEqualNumbers.java
+++ src/List/T01_SumAdjacentEqualNumbers.java
@@ -1,11 +1,36 @@
 package List;
 
+import java.lang.reflect.Array;
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class T01_SumAdjacentEqualNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        List<Double> numbers = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .map(Double::parseDouble)
+                .collect(Collectors.toList());
 
+        for (int i = 0; i < numbers.size() - 1; i++)
+            if (numbers.get(i).equals(numbers.get(i + 1))) {
+                numbers.set(i, numbers.get(i) + numbers.get(i + 1));
+                numbers.remove(i + 1);
+                i = -1;
+            }
+            String output = joinElementsByDelimiter(numbers);
+            System.out.println(output);
+
+    }
+
+    private static String joinElementsByDelimiter(List<Double> items) {
+        String output = "";
+        for (Double item : items)
+            output += (new DecimalFormat("0.#").format(item) + " ");
+        return output;
     }
 }
