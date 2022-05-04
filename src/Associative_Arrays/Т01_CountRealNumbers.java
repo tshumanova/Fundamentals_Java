9059f767da1f024a2d172563d9594bdb14032b96
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-04 16:38:03 +0300

    tasks

--- src/Associative_Arrays/Т01_CountRealNumbers.java
+++ src/Associative_Arrays/Т01_CountRealNumbers.java
@@ -1,9 +1,19 @@
 package Associative_Arrays;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Т01_CountRealNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        List<Double> numbersList = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .map(Double::parseDouble)
+                .collect(Collectors.toList());
+
+
     }
 }
