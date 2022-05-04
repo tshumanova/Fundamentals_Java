7c285d4827dd903ba464c4ca9758222502f3af22
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-04 16:36:51 +0300

    tasks

---
+++ src/Associative_Arrays/Т01_CountRealNumbers.java
@@ -0,0 +1,9 @@
+package Associative_Arrays;
+
+import java.util.Scanner;
+
+public class Т01_CountRealNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
