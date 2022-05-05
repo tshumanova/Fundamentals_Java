9ba71d538a23e6de0460756f170f66a1e3da450e
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-05 16:04:24 +0300

    tasks

---
+++ src/Associative_Arrays/T01_CountRealNumbers.java
@@ -0,0 +1,32 @@
+package Associative_Arrays;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class T01_CountRealNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Double> numbersList = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .map(Double::parseDouble)
+                .collect(Collectors.toList());
+
+        Map<Double, Integer> countNumbersMap = new TreeMap<>();
+
+
+        for (int i = 0; i < numbersList.size(); i++) {
+            double currentNum = numbersList.get(i);
+
+            if (countNumbersMap.containsKey(currentNum)) {
+                countNumbersMap.put(currentNum, countNumbersMap.get(currentNum) + 1);
+            } else {
+                countNumbersMap.put(currentNum, 1);
+            }
+        }
+        for (Map.Entry<Double, Integer> entry : countNumbersMap.entrySet()) {
+            System.out.printf("%.0f -> %d%n", entry.getKey(), entry.getValue());
+        }
+
+    }
+}
