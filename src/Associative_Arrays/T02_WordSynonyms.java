7771a00b3dfe10df5875dc311b3ae00ad4a97eda
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-07 23:31:33 +0300

    tasks

--- src/Associative_Arrays/T02_WordSynonyms.java
+++ src/Associative_Arrays/T02_WordSynonyms.java
@@ -1,7 +1,9 @@
 package Associative_Arrays;
 
+import java.util.Scanner;
+
 public class T02_WordSynonyms {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
     }
 }
