b19f49b3d10694265a1bba9973246018726dc843
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-09 12:56:37 +0300

    tasks

--- src/Associative_Arrays/T02_WordSynonyms.java
+++ src/Associative_Arrays/T02_WordSynonyms.java
@@ -1,10 +1,20 @@
 package Associative_Arrays;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class T02_WordSynonyms {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Map<String, List<String>> wordsMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+
+            String word = scanner.nextLine();
+            String synonym = scanner.nextLine();
+        }
     }
 }
