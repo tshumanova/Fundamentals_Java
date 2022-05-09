106a98351e407a94862ec6afefe83991207d70d0
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-09 13:05:11 +0300

    tasks

--- src/Text_Processing_Lab/T01_ReverseString.java
+++ src/Text_Processing_Lab/T01_ReverseString.java
@@ -5,5 +5,10 @@ import java.util.Scanner;
 public class T01_ReverseString {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        while (!input.equals("end")) {
+            String reversed = "";
+        }
     }
 }
