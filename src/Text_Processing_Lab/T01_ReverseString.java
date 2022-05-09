877c2622bebe9838578da925a5313fb214582cdb
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-09 13:02:34 +0300

    tasks

---
+++ src/Text_Processing_Lab/T01_ReverseString.java
@@ -0,0 +1,9 @@
+package Text_Processing_Lab;
+
+import java.util.Scanner;
+
+public class T01_ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
