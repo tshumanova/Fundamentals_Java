21dede000af42952b71420cc150edf247ac6741e
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-09 13:09:24 +0300

    tasks

--- src/Text_Processing_Lab/T01_ReverseString.java
+++ src/Text_Processing_Lab/T01_ReverseString.java
@@ -9,6 +9,11 @@ public class T01_ReverseString {
         String input = scanner.nextLine();
         while (!input.equals("end")) {
             String reversed = "";
+            for (int i = input.length() - 1; i >= 0; i--) {
+                reversed = reversed + input.charAt(i);
+            }
+            System.out.printf("%s = %s%n", input, reversed);
+            input = scanner.nextLine();
         }
     }
 }
