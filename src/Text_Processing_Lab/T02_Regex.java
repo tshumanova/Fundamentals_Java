7425bdadb6310fb93b876d58ce90e7a5c0121337
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-10 22:07:51 +0300

    tasks

--- src/Text_Processing_Lab/T02_Regex.java
+++ src/Text_Processing_Lab/T02_Regex.java
@@ -1,4 +1,9 @@
 package Text_Processing_Lab;
 
+import java.util.Scanner;
+
 public class T02_Regex {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
 }
