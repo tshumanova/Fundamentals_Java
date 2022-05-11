e243084c02751d5ad67ecda64d9aaec0fcab5530
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-11 15:33:30 +0300

    tasks

--- src/Text_Processing_Lab/T02_Regex.java
+++ src/Text_Processing_Lab/T02_Regex.java
@@ -1,10 +1,14 @@
 package Text_Processing_Lab;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class T02_Regex {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        String[] wordsArr = scanner.nextLine().split(" ");
+        List<String> resultList = new ArrayList<>();
     }
 }
