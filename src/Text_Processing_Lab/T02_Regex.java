62628ab0b94939cad2d7abd99e96b283c4522758
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-11 15:40:01 +0300

    tasks

--- src/Text_Processing_Lab/T02_Regex.java
+++ src/Text_Processing_Lab/T02_Regex.java
@@ -17,6 +17,11 @@ public class T02_Regex {
     }
 
     private static String repeatString(String word, int count) {
+        String[] repeatArr = new String[count];
+        for (int i = 0; i < count; i++) {
+            repeatArr[i] = word;
+        }
+        return String.join("", repeatArr);
     }
 
 
