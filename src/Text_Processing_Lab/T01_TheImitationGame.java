ee278bb7acfd6598d239b729c2060bf6fcb8eeb4
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-12 12:21:21 +0300

    tasks

--- src/Text_Processing_Lab/T01_TheImitationGame.java
+++ src/Text_Processing_Lab/T01_TheImitationGame.java
@@ -7,10 +7,21 @@ public class T01_TheImitationGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String massage = scanner.nextLine();
+
         String command = scanner.nextLine();
         while (!command.equals("Decode")) {
-            //TODO
-
+            String[] commandParts = command.split("\\|");
+            String commandName = commandParts[0];
+            switch (commandName) {
+                case "Move":
+                    break;
+                case "Insert":
+                    break;
+                case "ChangeAll":
+                    break;
+                default:
+                    throw new IllegalStateException("Unknown command" + commandName);
+            }
             command = scanner.nextLine();
         }
         System.out.println("The decrypted message is: " + massage);
