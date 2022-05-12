b1b658dc35572610a558c4f383e5f80f8f0d06fc
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-12 10:22:37 +0300

    tasks

---
+++ src/Text_Processing_Lab/T01_TheImitationGame.java
@@ -0,0 +1,11 @@
+package Text_Processing_Lab;
+
+import java.util.Scanner;
+
+public class T01_TheImitationGame {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
