6fbb93d70c88ec4593e2f79d3e37853f7ec6a589
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-12 12:34:34 +0300

    tasks

--- src/Text_Processing_Lab/T01_TheImitationGame.java
+++ src/Text_Processing_Lab/T01_TheImitationGame.java
@@ -6,7 +6,7 @@ public class T01_TheImitationGame {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String massage = scanner.nextLine();
+        String message = scanner.nextLine();
 
         String command = scanner.nextLine();
         while (!command.equals("Decode")) {
@@ -14,16 +14,30 @@ public class T01_TheImitationGame {
             String commandName = commandParts[0];
             switch (commandName) {
                 case "Move":
+                    message = move(message, Integer.parseInt(commandParts[1]));
                     break;
                 case "Insert":
+                    message = insert(message, Integer.parseInt(commandParts[1]), commandParts[2]);
                     break;
                 case "ChangeAll":
+                    message = changeAll(message, commandParts[1], commandParts[2]);
                     break;
                 default:
                     throw new IllegalStateException("Unknown command" + commandName);
             }
             command = scanner.nextLine();
         }
-        System.out.println("The decrypted message is: " + massage);
+        System.out.println("The decrypted message is: " + message);
     }
+
+    private static String insert(String message, int index, String substring) {
+
+        return null;
+    }
+
+    private static String move(String message, int parseInt) {
+        return null;
+    }
+
+
 }
