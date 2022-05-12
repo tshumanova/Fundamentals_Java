b1aa1977564f814e2fd69598ab3cfa49548c5b26
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-12 10:30:00 +0300

    tasks

--- src/Text_Processing_Lab/T01_TheImitationGame.java
+++ src/Text_Processing_Lab/T01_TheImitationGame.java
@@ -6,6 +6,13 @@ public class T01_TheImitationGame {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String massage = scanner.nextLine();
+        String command = scanner.nextLine();
+        while (!command.equals("Decode")) {
+            //TODO
 
+            command = scanner.nextLine();
+        }
+        System.out.println("The decrypted message is: " + massage);
     }
 }
