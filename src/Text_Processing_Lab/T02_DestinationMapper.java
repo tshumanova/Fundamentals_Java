4e96fc4d2be0a9e1453268a2db086b738e76132e
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-14 14:19:29 +0300

    tasks

--- src/Text_Processing_Lab/T02_DestinationMapper.java
+++ src/Text_Processing_Lab/T02_DestinationMapper.java
@@ -1,6 +1,9 @@
 package Text_Processing_Lab;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class T02_DestinationMapper {
@@ -8,7 +11,16 @@ public class T02_DestinationMapper {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
-        Pattern pattern= Pattern.compile("([=/]) (?<destination>[A-Z][a-z]{2,})\\1");
+        Pattern pattern = Pattern.compile("([=/]) (?<destination>[A-Z][a-z]{2,})\\1");
+        Matcher matcher = pattern.matcher(input);
+        List<String> destinations = new ArrayList<>();
+        boolean foundSomething = matcher.find();
+        while (foundSomething) {
+            System.out.println(matcher.group());
+
+            foundSomething = matcher.find();
+        }
+
         System.out.println("Destinations: Hawai, Cyprus");
         System.out.println("Travel Points : 0");
     }
