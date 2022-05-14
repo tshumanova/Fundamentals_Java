7a5af7e3c07d92c15e67884b78a3dc6719c0cf0e
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-14 14:12:08 +0300

    tasks

--- src/Text_Processing_Lab/T02_DestinationMapper.java
+++ src/Text_Processing_Lab/T02_DestinationMapper.java
@@ -1,9 +1,15 @@
 package Text_Processing_Lab;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class T02_DestinationMapper {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        Pattern pattern= Pattern.compile("");
+        System.out.println("Destinations: Hawai, Cyprus");
+        System.out.println("Travel Points : 0");
     }
 }
