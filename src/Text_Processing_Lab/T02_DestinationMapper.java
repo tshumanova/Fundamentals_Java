e0e0f99b9cc4192c50086626585fc589d1474d16
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-14 14:08:30 +0300

    tasks

--- src/Text_Processing_Lab/T02_DestinationMapper.java
+++ src/Text_Processing_Lab/T02_DestinationMapper.java
@@ -1,7 +1,9 @@
 package Text_Processing_Lab;
 
+import java.util.Scanner;
+
 public class T02_DestinationMapper {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
     }
 }
