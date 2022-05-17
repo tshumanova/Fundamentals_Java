b7510fd7b8b82e7baa22233915b2283f274edf8c
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-17 16:04:49 +0300

    tasks

--- src/Text_Processing_Lab/T01_text.java
+++ src/Text_Processing_Lab/T01_text.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class T01_text {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        StringBuilder digits=new StringBuilder();
 
     }
 }
