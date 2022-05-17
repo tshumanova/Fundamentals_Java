fce4b98cf78bcb26871c11c224190645a74d0aa3
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-17 16:20:35 +0300

    tasks

--- src/Text_Processing_Lab/T01_text.java
+++ src/Text_Processing_Lab/T01_text.java
@@ -9,5 +9,18 @@ public class T01_text {
         StringBuilder digits = new StringBuilder();
         StringBuilder letter = new StringBuilder();
         StringBuilder other = new StringBuilder();
-    }
+
+        for (int i = 0; i < text.length(); i++) {
+            char symbol = text.charAt(i);
+
+            if (Character.isDigit(symbol)) {
+                digits.append(symbol);
+            } else if (Character.isLetter(symbol)) {
+                letter.append(symbol);
+            } else {
+                other.append(symbol);
+            }
+        }
+
+            }
 }
